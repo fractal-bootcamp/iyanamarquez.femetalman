e9860f8e46c8c758cb92f6885449f578d855e54c
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-08-19 18:09:19 -0400

    build out card select functionality

--- src/mockRoslib.js
+++ src/mockRoslib.js
@@ -1,45 +1,49 @@
-// mockRoslib.js
 class MockRos {
-    constructor() {
-        console.log('Mock ROS instance created');
+  constructor() {
+    console.log("Mock ROS instance created");
+  }
+  on(event, callback) {
+    if (event === "connection") {
+      console.log("Mock: Connected to ROS");
+      callback();
+    } else if (event === "error") {
+      console.log("Mock: Error connecting to ROS");
+    } else if (event === "close") {
+      console.log("Mock: Connection to ROS closed");
     }
-    on(event, callback) {
-        if (event === 'connection') {
-            console.log('Mock: Connected to ROS');
-            callback();
-        } else if (event === 'error') {
-            console.log('Mock: Error connecting to ROS');
-        } else if (event === 'close') {
-            console.log('Mock: Connection to ROS closed');
-        }
-    }
-    close() {
-        console.log('Mock: ROS connection closed');
-    }
+  }
+  close() {
+    console.log("Mock: ROS connection closed");
+  }
 }
 
 class MockTopic {
-    constructor({ ros, name, messageType }) {
-        this.name = name;
-        this.messageType = messageType;
-        console.log(`Mock Topic created: ${name}`);
-    }
-    subscribe(callback) {
-        console.log('Mock: Subscribed to topic');
-        // Simulate receiving a message after 2 seconds
-        setTimeout(() => {
-            callback({ data: 'Hello from Mock ROS!' });
-        }, 2000);
-    }
-    unsubscribe() {
-        console.log('Mock: Unsubscribed from topic');
-    }
-    publish(message) {
-        console.log('Mock: Published message:', message);
-    }
+  constructor({ ros, name, messageType }) {
+    this.name = name;
+    this.messageType = messageType;
+    this.subscribers = []; // Track subscribers
+    // console.log(`Mock Topic created: ${name}`);
+  }
+  subscribe(callback) {
+    console.log("Mock: Subscribed to topic", callback);
+    this.subscribers.push(callback);
+    // Simulate receiving a message after 2 seconds
+    setTimeout(() => {
+      callback({ data: "Hello from Mock ROS!" });
+    }, 2000);
+  }
+  unsubscribe() {
+    console.log("Mock: Unsubscribed from topic");
+    this.subscribers = []; // Clear subscribers
+  }
+  publish(message) {
+    console.log("Mock: Published message:", message);
+    // Notify all subscribers with the message
+    this.subscribers.forEach((callback) => callback(message));
+  }
 }
 
 export default {
-    Ros: MockRos,
-    Topic: MockTopic
+  Ros: MockRos,
+  Topic: MockTopic,
 };
