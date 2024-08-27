d359e391bd855b9e8f83de6456b13b04d85a90b2
Iyana M <yayakix@gmail.com>
2024-08-27 22:15:32 +0000

    move in image stream

--- src/Layouts/ProgramLayout.tsx
+++ src/Layouts/ProgramLayout.tsx
@@ -1,10 +1,12 @@
 import ArmCard from "../Components/ArmCard/ArmCard";
 import GridTable from "../Components/GridTable/GridTable";
 import JoystickComponent from "../Components/GridTable/Joystick";
+import ROSImageStream from "../RosImageStream";
 
 const ProgramLayout = () => {
   return (
     <div className="pt-10">
+      <ROSImageStream streamUrl=""/>
       <JoystickComponent />
       <div className="flex flex-row justify-around items-center space-x-4 ">
         <GridTable />
